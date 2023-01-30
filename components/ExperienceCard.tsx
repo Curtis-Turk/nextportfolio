5954c8014bd8c94647a182bfd17a909d298e41ca
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-30 16:16:54 +0000

    Bugfixing - ExperienceCard still has issues

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   company: string;
@@ -7,6 +9,7 @@ type Props = {
   dates: string;
   responsibilities: Array<string>;
   logo: string;
+  link: string;
 };
 
 export default function ExperienceCard({
@@ -15,22 +18,25 @@ export default function ExperienceCard({
   dates,
   responsibilities,
   logo,
+  link,
 }: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] lg:w-[900px] snap-center bg-[#292929] p-10">
-      <motion.img
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] lg:w-[900px] snap-center bg-[#292929] p-12">
+      <motion.div
         initial={{ x: -200, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 2 }}
         viewport={{ once: true }}
         className="w-50 h-32 object-cover object-center"
-        src={logo}
-        alt="Cylery Logo"
-      ></motion.img>
-      <div>
+      >
+        <Link href={link}>
+          <Image src={logo} alt="" fill></Image>
+        </Link>
+      </motion.div>
+      <div className="">
         <h4 className="text-4xl font-light">{jobTitle}</h4>
         <h4>{company}</h4>
-        <p>{dates}</p>
+        <p className="font-light">{dates}</p>
         <ul className="space-y-4">
           <li>{responsibilities[0]}</li>
           <li>{responsibilities[1]}</li>
